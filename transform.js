fda504998c69c544cb92a2b74a3158dd57a9778a
Tom Dale <tom@tomdale.net>
2016-10-16 11:54:46 -0400

    Handle reserved words and default imports

--- transform.js
+++ transform.js
@@ -1,4 +1,5 @@
 const mappings = require("./mapping");
+const reserved = require("./reserved");
 const j = require("jscodeshift");
 
 class Module {
@@ -38,7 +39,6 @@ module.exports = function (file, api, options) {
   let j = api.jscodeshift;
 
   let root = j(file.source);
-  let body = root.get().value.program.body;
 
   let replacements = root.find(j.MemberExpression, { object: { name: "Ember" } })
     .filter(isEmberGlobal)
@@ -52,6 +52,7 @@ module.exports = function (file, api, options) {
 
   // importModules(root, modules);
 
+  console.log(root.toSource());
   return root.toSource();
 
   function findReplacement(path) {
@@ -81,7 +82,11 @@ module.exports = function (file, api, options) {
 
     if (!mapping.mod.local) {
       // Ember.computed.or => or
-      mapping.mod.local = propertyPath.split(".").slice(-1)[0];
+      let local = propertyPath.split(".").slice(-1)[0];
+      if (reserved.includes(local)) {
+        local = `Ember${local}`;
+      }
+      mapping.mod.local = local;
     }
 
     let replacement = new Replacement(nodePath, mapping);
@@ -97,16 +102,33 @@ module.exports = function (file, api, options) {
 }
 
 function updateOrCreateImportDeclarations(root, usedModules, existingModules) {
+  let body = root.get().value.program.body;
+
   usedModules.forEach(usedMod => {
     let bySource;
+    let { imported } = usedMod;
 
     if (bySource = findExisting(usedMod)) {
-      if (!bySource[usedMod.imported]) {
-        let specifier = j.importSpecifier(usedMod.imported, usedMod.local);
+      if (!bySource[imported]) {
+        let specifier;
+        if (imported === 'default') {
+          specifier = j.importDefaultSpecifier(j.identifier(usedMod.local));
+        } else {
+          specifier = j.importSpecifier(j.identifier(usedMod.imported), j.identifier(usedMod.local));
+        }
+
         root.find(j.ImportDeclaration, {
           source: { value: usedMod.source }
         }).get("specifiers").push(specifier);
       }
+    } else {
+      let importStatement = createImportStatement(usedMod.source, usedMod.imported, usedMod.local);
+      body.unshift(importStatement);
+      body[0].comments = body[1].comments;
+      delete body[1].comments;
+      usedMod.node = importStatement;
+      existingModules[usedMod.source] = {};
+      existingModules[usedMod.source][usedMod.imported] = usedMod;
     }
   });
 
@@ -166,16 +188,8 @@ function findExistingModules(root) {
     });
 
   return modulesBySource;
-
-  // let importStatement = createImportStatement(pkg, identifier, exportName);
-  // addImport(body, importStatement);
 }
 
-function addImport(body, importStatement) {
-  body.unshift(importStatement);
-  body[0].comments = body[1].comments;
-  delete body[1].comments;
-}
 
 function expandMemberExpressions(path) {
   let propName = path.node.property.name;
@@ -196,36 +210,36 @@ function expandMemberExpressions(path) {
 }
 
 // Flagrantly stolen from https://github.com/5to6/5to6-codemod/blob/master/utils/main.js
-function createImportStatement(moduleName, variableName, propName) {
+function createImportStatement(source, imported, local) {
   var declaration, variable, idIdentifier, nameIdentifier;
   // console.log('variableName', variableName);
   // console.log('moduleName', moduleName);
 
   // if no variable name, return `import 'jquery'`
-  if (!variableName) {
-    declaration = j.importDeclaration([], j.literal(moduleName));
+  if (!local) {
+    declaration = j.importDeclaration([], j.literal(source));
     return declaration;
   }
 
   // multiple variable names indicates a destructured import
-  if (Array.isArray(variableName)) {
-    var variableIds = variableName.map(function (v) {
+  if (Array.isArray(local)) {
+    var variableIds = local.map(function (v) {
       return j.importSpecifier(j.identifier(v), j.identifier(v));
     });
 
-    declaration = j.importDeclaration(variableIds, j.literal(moduleName));
+    declaration = j.importDeclaration(variableIds, j.literal(source));
   } else {
     // else returns `import $ from 'jquery'`
-    nameIdentifier = j.identifier(variableName); //import var name
+    nameIdentifier = j.identifier(local); //import var name
     variable = j.importDefaultSpecifier(nameIdentifier);
 
     // if propName, use destructuring `import {pluck} from 'underscore'`
-    if (propName) {
-      idIdentifier = j.identifier(propName);
+    if (imported && imported !== "default") {
+      idIdentifier = j.identifier(imported);
       variable = j.importSpecifier(idIdentifier, nameIdentifier); // if both are same, one is dropped...
     }
 
-    declaration = j.importDeclaration([variable], j.literal(moduleName));
+    declaration = j.importDeclaration([variable], j.literal(source));
   }
 
   return declaration;
