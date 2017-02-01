4a644bef4d78e7a087f9a35b1e2cfbb5439415bf
Craig Bilner <craig.bilner@ig.com>
2017-02-01 17:59:56 +0000

    Add tests Add alias check

---
+++ test/expected-output/existing-alias.js
@@ -0,0 +1,9 @@
+import EmberObject from "@ember/object";
+import EmberHelper from "@ember/component/helper";
+import Ember from 'ember';
+
+const Helper = EmberHelper || EmberObject;
+
+export default Helper.extend({
+
+});
